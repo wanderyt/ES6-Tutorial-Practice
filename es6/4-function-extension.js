33104f39da990ee050edf389aeb6731ad048b834
David Ren <david.ren@sap.com>
2016-03-29 14:27:49 +0800

    Update function extension note.

--- es6/4-function-extension.js
+++ es6/4-function-extension.js
@@ -8,3 +8,19 @@ var fn2 = function(x, y = 2) {
 
 console.log("fn1.length: " + fn1.length);
 console.log("fn2.length: " + fn2.length);
+
+// Context
+var a = [1, 2, 3, 4];
+var test1 = function() {
+	return this.a;
+};
+
+test1(); // [1, 2, 3, 4]
+
+test1.prototype.call([1,2,3]); // [1, 2, 3]
+
+var test2 = () => this.a;
+
+test2(); // [1, 2, 3, 4]
+
+fn2.call({a: [1,2,3]}); // [1, 2, 3, 4]
